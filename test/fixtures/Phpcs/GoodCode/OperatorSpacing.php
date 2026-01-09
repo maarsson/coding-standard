ae2ef36bb1cad32f28b3e1c417bfeb14797dda58
VMaarsson <viktor@maarsson.se>
2026-01-09 15:31:18 +0100

    test: add testable files

---
+++ test/fixtures/Phpcs/GoodCode/OperatorSpacing.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpcs\GoodCode;
+
+final class OperatorSpacing
+{
+    public function run(): int
+    {
+        $number = 123;
+
+        return $number;
+    }
+}
