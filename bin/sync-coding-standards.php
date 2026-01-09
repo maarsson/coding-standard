dae9331e8c6d8ba7311c0455fc77388b2f3b841e
VMaarsson <viktor@maarsson.se>
2026-01-09 11:14:52 +0100

    feat: add installer script

---
+++ bin/sync-coding-standards.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+$projectRoot = getcwd(); // when executed from app, this is app root
+$vendorSource = $projectRoot . '/vendor/maarsson/coding-standard/src/phpmd.xml.dist';
+$target = $projectRoot . '/phpmd.xml';
+
+if (! file_exists($vendorSource)) {
+    fwrite(STDERR, "Source file not found: {$vendorSource}\n");
+    exit(1);
+}
+
+if (! copy($vendorSource, $target)) {
+    fwrite(STDERR, "Failed to copy phpmd.xml to project root.\n");
+    exit(1);
+}
+
+fwrite(STDOUT, "Coding standard rulesets are applied to project.\n");
+exit(0);
