83157880baafd2d997ef39d6f53b2987a2b652e2
VMaarsson <viktor@maarsson.se>
2026-01-09 15:18:11 +0100

    refactor: rename variables

--- bin/sync-coding-standards.php
+++ bin/sync-coding-standards.php
@@ -4,15 +4,16 @@
 declare(strict_types=1);
 
 $projectRoot = getcwd(); // when executed from app, this is app root
-$vendorSource = $projectRoot . '/vendor/maarsson/coding-standard/resources/phpmd.xml.dist';
-$target = $projectRoot . '/phpmd.xml';
 
-if (! file_exists($vendorSource)) {
-    fwrite(STDERR, "Source file not found: {$vendorSource}\n");
+$vendorSourcePhpmd = $projectRoot . '/vendor/maarsson/coding-standard/resources/phpmd.xml.dist';
+$targetPhpmd = $projectRoot . '/phpmd.xml';
+
+if (! file_exists($vendorSourcePhpmd)) {
+    fwrite(STDERR, "Source file not found: {$vendorSourcePhpmd}\n");
     exit(1);
 }
 
-if (! copy($vendorSource, $target)) {
+if (! copy($vendorSourcePhpmd, $targetPhpmd)) {
     fwrite(STDERR, "Failed to copy phpmd.xml to project root.\n");
     exit(1);
 }
