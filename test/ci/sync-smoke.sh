618babacdf05a6e4b29a6d9cfc20b512475dbcd2
VMaarsson <viktor@maarsson.se>
2026-01-10 13:49:43 +0100

    test: add fail fallbacks

--- test/ci/sync-smoke.sh
+++ test/ci/sync-smoke.sh
@@ -8,7 +8,7 @@ ok() {
   printf "[\033[32m OK \033[0m] %s\n" "$1"
 }
 fail() {
-  printf "[\033[31mFAIL\033[0m] %s\n" "$1"
+  printf "[\033[31mFAIL\033[0m] %s\n" "$1" >&2;exit 1;
 }
 
 WORKDIR="/tmp/sync-smoke"
@@ -53,16 +53,16 @@ info "Running sync script…"
 ./vendor/bin/sync-coding-standards.php
 
 info "Asserting files exist in project root…"
-test -f phpmd.xml
-test -f phpcs.xml
+test -f phpmd.xml || fail "phpmd.xml was not copied to project root"
+test -f phpcs.xml || fail "phpcs.xml was not copied to project root"
 
 info "Asserting files match package dist versions…"
-cmp -s phpmd.xml ./vendor/maarsson/coding-standard/resources/phpmd.xml.dist
-cmp -s phpcs.xml ./vendor/maarsson/coding-standard/resources/phpcs.xml.dist
+cmp -s phpmd.xml ./vendor/maarsson/coding-standard/resources/phpmd.xml.dist || fail "phpmd.xml mismatch error"
+cmp -s phpcs.xml ./vendor/maarsson/coding-standard/resources/phpcs.xml.dist || fail "phpcs.xml mismatch error"
 
 info "Asserting always-overwrite behavior…"
 echo "local change" >> phpmd.xml
 ./vendor/bin/sync-coding-standards.php
-cmp -s phpmd.xml ./vendor/maarsson/coding-standard/resources/phpmd.xml.dist
+cmp -s phpmd.xml ./vendor/maarsson/coding-standard/resources/phpmd.xml.dist || fail "phpcs.xml overwrite error"
 
 ok "Sync smoke test passed."
